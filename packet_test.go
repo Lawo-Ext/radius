8ad4fb03b81e36c9c7e9f9233246e2f8f233b7dd
jamie <jamie.macpherson@lawo.com>
2019-09-19 10:34:44 +0100

    Radius: Add EncodeOriginal func

--- packet_test.go
+++ packet_test.go
@@ -3,6 +3,7 @@ package radius_test
 import (
 	"bytes"
 	"encoding/hex"
+	"log"
 	"net"
 	"strings"
 	"testing"
@@ -341,4 +342,16 @@ func RADIUSPacketsEqual(a, b []byte) bool {
 	}
 
 	return true
+}
+
+func TestPacket_EncodeOriginal(t *testing.T) {
+
+	p := radius.New(radius.CodeAccessRequest, []byte(`12345`))
+
+	if result, err := p.EncodeOriginal(); err != nil {
+		t.Fatalf("expected no error got %s \n", err)
+	} else {
+		log.Printf("result:0x%x \n", result)
+	}
+
 }
